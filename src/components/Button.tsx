dd67c84801b2b2faf34d214a33131e50d81c79bf
yadira <yadiraco304@gmail.com>
2022-08-05 09:45:28 -0500

    Fix v6

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({}) => {
-  return <button style={{ color: "green" }}>Button of Example</button>;
+  return <button style={{ color: "blue" }}>Button of Example</button>;
 };
 
 export { Button };
