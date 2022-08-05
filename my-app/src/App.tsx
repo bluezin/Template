0c87c9a40d1ee260c2efc21bc5e85b386b90ea31
yadira <yadiraco304@gmail.com>
2022-08-05 09:42:57 -0500

    Fix v5

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import React from 'react';
+import { Button } from 'components'
 
 function App() {
   return (
     <div className="App">
       <h1>Prueba de componentes</h1>
+      <Button />
     </div>
   );
 }
